01e97c702cdab787ec8a4183d34ca936ad17b8f8
Valentin RENIER <valentin.renier@etu.esisar.grenoble-inp.fr>
2024-11-03 22:27:07 +0000

    adding the display of already created tables

--- app/apis/ui.py
+++ app/apis/ui.py
@@ -1,16 +1,22 @@
 import sys
 from flask import make_response, render_template
 from flask_restx import Namespace,Resource
-from services.db_service import get_restaurant
+from services.db_service import get_restaurant, get_all_tables
 
 api=Namespace("ui",description="UI-related endpoints")
 
 @api.route("/restaurant/<int:id>")
 class RestaurantPage(Resource):
     def get(self,id):
-        restaurant,_=get_restaurant(id)
+        tables = get_all_tables(id)
+        restaurant = get_restaurant(id)
         if restaurant is None:
-            return "Restaurant not found",404
+            return make_response("No restaurant found", 404)
+        if tables is None:
+            return make_response("No tables for this restaurant", 404)
         else:
-            print(restaurant,file=sys.stderr)
-            return make_response(render_template("restaurant.html",restaurant=restaurant),200,{'Content-Type': 'text/html'})
+            return make_response(
+                render_template("restaurant.html", restaurant=restaurant, tables=tables),
+                200,
+                {'Content-Type': 'text/html'}
+            )
