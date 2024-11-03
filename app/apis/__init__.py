d600552b8730c9749f2ed8cbbe3725bb89a42553
BrunoPascoa76 <brunopascoa03@ua.pt>
2024-11-03 09:19:05 +0000

    changed api prefix

--- app/apis/__init__.py
+++ app/apis/__init__.py
@@ -3,7 +3,7 @@ from flask_restx import Api
 
 blueprint = Blueprint('apis', __name__)
 
-api = Api(version="1.0",title="TopTable Staff API",description="TopTable API for the staff side",prefix="/api/v1")
+api = Api(version="1.0",title="TopTable Staff API",description="TopTable API for the staff side",prefix="/")
 
 from .auth import api as api_auth
 from .layout import api as api_layout
