154dcb7d53c22dffdc036bfef434de6929fc0891
ilkeratik <ilkeratik35@gmail.com>
2024-11-14 20:15:10 +0000

    use ROOT_PATH_PREFIX for swagger ui address as well

--- app/apis/__init__.py
+++ app/apis/__init__.py
@@ -3,9 +3,13 @@ from flask_restx import Api
 
 from secret import ROOT_PATH_PREFIX
 
-blueprint = Blueprint('apis', __name__)
+blueprint = Blueprint('apis', __name__, url_prefix=ROOT_PATH_PREFIX)
 
-api = Api(version="1.0",title="TopTable Staff API",description="TopTable API for the staff side",prefix=ROOT_PATH_PREFIX)
+api = Api(version="1.0",
+          title="TopTable Staff API",
+          description="TopTable API for the staff side",
+          prefix=ROOT_PATH_PREFIX,
+          doc=f'{ROOT_PATH_PREFIX}' )
 
 from .auth import api as api_auth
 from .layout import api as api_layout
