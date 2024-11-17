b0581dda1c0efe3cb2228b4c99c609db43d4c0f2
robinlafage <robinlafage42@gmail.com>
2024-11-17 21:17:16 +0000

    test

--- app/apis/ui.py
+++ app/apis/ui.py
@@ -7,7 +7,6 @@ from services.db_service import get_reservations, get_restaurant, get_all_tables
 
 api=Namespace("ui",description="UI-related endpoints")
 
-
 @api.route("/restaurant/<int:id>")
 class RestaurantPage(Resource):
     def get(self, id):
