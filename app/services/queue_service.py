c8557d64fde780ef24f6259c59b9f4b27bfd3818
Valentin RENIER <valentin.renier@etu.esisar.grenoble-inp.fr>
2024-12-01 21:08:15 +0000

    Adding the possibility to create a restaurant

--- app/services/queue_service.py
+++ app/services/queue_service.py
@@ -3,7 +3,7 @@ import os
 import boto3
 
 
-def get_reservation_confirmation(restaurant_id_list,sqs=boto3.client('sqs', region_name='us-east-1'),queue_url=os.getenv("SQS_RESERVATION_RESQUESTS_QUEUE_URL")):
+def get_reservation_confirmation(restaurant_id_list,sqs=boto3.client('sqs'),queue_url=os.getenv("SQS_RESERVATION_RESQUESTS_QUEUE_URL")):
     while True:
         response=sqs.receive_message(
             QueueUrl=queue_url,
@@ -18,7 +18,7 @@ def get_reservation_confirmation(restaurant_id_list,sqs=boto3.client('sqs', regi
                     return message
 
 
-def delete_reservation_confirmation(receipt_handle,sqs=boto3.client('sqs', region_name='us-east-1'),queue_url=os.getenv("QUEUE_URL")):
+def delete_reservation_confirmation(receipt_handle,sqs=boto3.client('sqs'),queue_url=os.getenv("QUEUE_URL")):
     sqs.delete_message(
         QueueUrl=queue_url,
         ReceiptHandle=receipt_handle
